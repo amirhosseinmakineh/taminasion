dd2d527997f59fe9e4c24198f850bac85a7e7085
amirhosseinmakineh <75474193+amirhosseinmakineh@users.noreply.github.com>
2025-08-28 16:26:11 +0330

    feat: add business modules and improve modal

---
+++ src/app/Components/BusinessProfile/BusinessProfile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-profile',
+  templateUrl: './BusinessProfile.component.html',
+  styleUrls: ['./BusinessProfile.component.css'],
+  standalone: false,
+})
+export class BusinessProfileComponent {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'business-search',
-    loadChildren: () => import('./Components/BusinessSearch/business-search.module').then(m => m.BusinessSearchModule)
-  },
-  {
-    path: 'businesssearch',
-    redirectTo: 'business-search',
-    pathMatch: 'full'
-  },
+    {
+      path: 'business-search',
+      loadChildren: () => import('./Components/BusinessSearch/business-search.module').then(m => m.BusinessSearchModule)
+    },
+    {
+      path: 'business-details',
+      loadChildren: () => import('./Components/BusinessDetails/business-details.module').then(m => m.BusinessDetailsModule)
+    },
+    {
+      path: 'business-profile',
+      loadChildren: () => import('./Components/BusinessProfile/business-profile.module').then(m => m.BusinessProfileModule)
+    },
+    {
+      path: 'businesssearch',
+      redirectTo: 'business-search',
+      pathMatch: 'full'
+    },
   {
     path: '',
     loadChildren: () => import('./Components/MainPage/main-page.module').then(m => m.MainPageModule)
